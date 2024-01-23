c417d504f8d1f0dc9000c005309a27c66b21a27a
M_H_MISU <83594748+Webdevelopermisu@users.noreply.github.com>
2024-01-23 18:58:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,214 @@
+let inputDir = {x:0,y:0};
+
+const foodSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
+
+var speed = 7;
+
+let score = 0;
+let lastPaintTime = 0;
+let snakeArr = [
+    {x:13,y:15}
+]
+
+food = {x: 16,y: 17};
+
+//game function
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    
+    if((ctime - lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+}
+
+
+function isCollide(snake) {
+    // if you bump yourself
+    for (let i = 1; i < snakeArr.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+            return true;
+        }
+
+    }
+
+    // if you bump the wall
+
+        if (snake[0].x >= 36 || snake[0].x <= 0 || snake[0].y >= 36 || snake[0].y <= 0) {
+            return true;
+        
+    }
+}
+
+
+function gameEngine() {
+    // part 1 : update game
+
+    if (isCollide(snakeArr)) {
+        gameOverSound.play();
+        musicSound.pause();
+        inputDir = {x:0,y:0};
+        alert("Game over press Enter key to play again...!")
+        snakeArr = [{x:13,y:15}];
+        musicSound.play();
+        score = 0;
+
+    }
+    /// if you have iffen the food incriment the score and reganarate the food
+
+    if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+        foodSound.play();
+        score +=1;
+        if (score>hiscoreval) {
+            hiscoreval = score;
+            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+            hiscoreBox.innerHTML = "Highscore : " + hiscoreval;
+        }
+        scoreBox.innerHTML = "Score :" + score;
+        snakeArr.unshift({x:snakeArr[0].x + inputDir.x, y:snakeArr[0].y + inputDir.y});
+        let a = 2;
+        let b = 34;
+        food = {x: Math.round(a + (b - a)* Math.random()), y: Math.round(a + (b - a)* Math.random())};
+    }
+
+    // moving the snake
+
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i+1] = {...snakeArr[i]};
+    }
+    
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
+
+    //part 2 : display the food
+
+
+
+    //part 2 : display the snake
+
+    board.innerHTML = "";
+    
+    snakeArr.forEach((e, index)=>{
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        if(index === 0){
+            snakeElement.classList.add('head');
+        }else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement);
+    });
+
+    // display the food
+    
+        foodElement = document.createElement('div');
+        foodElement.style.gridRowStart = food.y;
+        foodElement.style.gridColumnStart = food.x;
+        foodElement.classList.add('food');
+        board.appendChild(foodElement);
+
+
+}
+
+
+
+
+
+//main logic
+
+
+let hiscore = localStorage.getItem("hiscore");
+if (hiscore === null) {
+    hiscoreval = 0;
+    localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
+}else{
+    hiscoreval = JSON.parse(hiscore);
+    hiscoreBox.innerHTML = "Highscore : " + hiscore;
+}
+
+
+window.requestAnimationFrame(main);
+
+
+
+window.addEventListener('keydown',e=>{
+    inputDir = {x:0,y:1}//start game
+    moveSound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            
+            inputDir.x = 0;
+            inputDir.y = -1;
+            break;
+        case "ArrowDown":
+            
+            inputDir.x = 0;
+            inputDir.y = 1;
+            break;
+        case "ArrowLeft":
+            
+            inputDir.x = -1;
+            inputDir.y = 0;
+            break;
+        case "ArrowRight":
+            
+            inputDir.x = 1;
+            inputDir.y = 0;
+            break;
+    
+        default:
+            break;
+    }
+})
+
+
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu")
+}
+
+
+function easy() {
+    speed = 5;
+    button.style.color = "red"
+}
+function mediam() {
+    speed = 14;
+}
+function hard() {
+    speed = 18;
+}
+
+
+
+function openpopBox() {
+    let popBox = document.getElementById('popBox');
+
+    popBox.style.display = "block"
+}
+
+function closepopBox() {
+    let popBox = document.getElementById('popBox');
+
+    popBox.style.display = "none"
+}
+
+
+function playMusic() {
+    musicSound.play()
+}
+function puseMusic() {
+    musicSound.pause()
+
+}
+
+
+
+alert("This game plays only pc or laptop..!")
